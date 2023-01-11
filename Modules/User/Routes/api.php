091bd9b27384a608ab66d886734fb44cdc940b58
miladev-ent <milwad.dev@gmail.com>
2023-01-11 17:02:54 +0330

    add UserService

--- Modules/User/Routes/api.php
+++ Modules/User/Routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\User\Http\Controllers\UserController;
 
 /*
  * User routes.
---
+++ Modules/User/Services/UserService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\User\Services;
+
+class UserService
+{
+
+}
