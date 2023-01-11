6cfcb13f21d640e7c2eaddb16cbb45b370f7af52
miladev-ent <milwad.dev@gmail.com>
2023-01-11 17:09:18 +0330

    add json response

---
+++ Modules/Common/Contracts/JsonResponseInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\Common\Contracts;
+
+/**
+ * 
+ */
+interface JsonResponseInterface {}
---
+++ Modules/Common/Responses/JsonResponse.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\Common\Responses;
+
+class JsonResponse
+{
+
+}
