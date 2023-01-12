778b1fba27ffa3187320b077d75764c5b5710c9c
miladev-ent <milwad.dev@gmail.com>
2023-01-12 15:35:24 +0330

    add successResponse method

--- Modules/Common/Responses/JsonResponse.php
+++ Modules/Common/Responses/JsonResponse.php
@@ -4,5 +4,16 @@ namespace Modules\Common\Responses;
 
 class JsonResponse
 {
-
+    /**
+     * Return success json response.
+     *
+     * @param array|string|int|object $data
+     * @param array $headers
+     * @param int $options
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function successResponse(array|string|int|object $data, array $headers = [], int $options = 0)
+    {
+        return response()->json(data: $data, headers: $headers, options: $options);
+    }
 }
