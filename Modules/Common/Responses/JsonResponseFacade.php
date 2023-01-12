f0e04d9fa39ae9d09df8dda459eb4f5dd189a3de
miladev-ent <milwad.dev@gmail.com>
2023-01-12 15:39:11 +0330

    add JsonResponseFacade

---
+++ Modules/Common/Responses/JsonResponseFacade.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Common\Responses;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ *
+ */
+class JsonResponseFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'json-response';
+    }
+}
