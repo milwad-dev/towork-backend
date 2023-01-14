d34a1162efa9236dff8d1997ffc941df3a964f11
miladev-ent <milwad.dev@gmail.com>
2023-01-14 19:21:39 +0330

    register user migration files

--- Modules/User/Providers/UserServiceProvider.php
+++ Modules/User/Providers/UserServiceProvider.php
@@ -6,5 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class UserServiceProvider extends ServiceProvider
 {
-
+    /**
+     * Register files.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
 }
