a5f7be8e3045023f77f247e068b41fbdeae6db97
miladev-ent <milwad.dev@gmail.com>
2023-01-19 16:35:39 +0330

    add UserSingleResource

---
+++ Modules/User/Http/Resources/UserSingleResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\User\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserSingleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
+    }
+}
