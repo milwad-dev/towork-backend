0679aa3eee52851e7696d291aba5a57c0bac8b3d
miladev-ent <milwad.dev@gmail.com>
2023-01-19 16:37:48 +0330

    add store method in user service

--- Modules/User/Services/UserService.php
+++ Modules/User/Services/UserService.php
@@ -2,7 +2,23 @@
 
 namespace Modules\User\Services;
 
+use Illuminate\Support\Facades\Hash;
+use Modules\User\Models\User;
+
 class UserService
 {
-
+    /**
+     * Store user by array of data.
+     *
+     * @param array $data
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model
+     */
+    public function store(array $data)
+    {
+        return User::query()->create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+    }
 }
