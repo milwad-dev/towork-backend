5c18677f7de2c2f549b9ddc37f1756dfe6236a0a
miladev-ent <milwad.dev@gmail.com>
2023-01-22 15:29:25 +0330

    install & configuration swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(title="To-Work-Backend", version="0.1")
+ * @OA\PathItem(path="/api")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
