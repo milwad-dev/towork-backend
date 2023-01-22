d0922e74f3d446b827c115dc132ffee791bc4a58
miladev-ent <milwad.dev@gmail.com>
2023-01-22 15:11:17 +0330

    delete store method in user service

--- Modules/User/Services/UserService.php
+++ Modules/User/Services/UserService.php
@@ -8,21 +8,6 @@ use Modules\User\Models\User;
 class UserService
 {
     /**
-     * Store user by array of data.
-     *
-     * @param array $data
-     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model
-     */
-    public function store(array $data)
-    {
-        return User::query()->create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password'])
-        ]);
-    }
-
-    /**
      * Update user by array of data and id.
      *
      * @param array $data
