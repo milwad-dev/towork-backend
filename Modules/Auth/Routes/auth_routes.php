f15822402281c0f84df60095ba4edeefdebd315e
miladev-ent <milwad.dev@gmail.com>
2023-01-24 20:08:58 +0330

    fix auth route

--- Modules/Auth/Routes/auth_routes.php
+++ Modules/Auth/Routes/auth_routes.php
@@ -5,7 +5,7 @@ use Modules\Auth\Http\Controllers\LoginController;
 use Modules\Auth\Http\Controllers\RegisterController;
 
 Route::group([], static function ($router) {
-    $router->post('register', [RegisterController::class]);
+    $router->post('register', RegisterController::class);
 
-    $router->post('login', [LoginController::class]);
+    $router->post('login', LoginController::class);
 });
