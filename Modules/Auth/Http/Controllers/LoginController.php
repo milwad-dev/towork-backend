067a624e9a32375d7ae78a74a9a340f4cbd47d84
miladev-ent <milwad.dev@gmail.com>
2023-01-24 20:05:15 +0330

    complete login method

--- Modules/Auth/Http/Controllers/LoginController.php
+++ Modules/Auth/Http/Controllers/LoginController.php
@@ -2,12 +2,34 @@
 
 namespace Modules\Auth\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Modules\Auth\Http\Requests\LoginRequest;
 use Modules\Common\Http\Controllers\Controller;
+use Modules\User\Services\UserService;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends Controller
 {
-    public function __invoke()
+    public function __invoke(LoginRequest $request)
     {
+        $field = $request->email;
+        $correctField = filter_var($field, FILTER_VALIDATE_EMAIL) ? 'email' : 'phone';
 
+        if (! Auth::attempt([$correctField => $field, 'password' => $request->password])) {
+            return response([
+                'data' => ['message' => 'Unauthorised.'],
+                'status' => 'error'
+            ], Response::HTTP_FORBIDDEN);
+        }
+
+        $user = Auth::user();
+        return response([
+            'data' => [
+                'token' => resolve(UserService::class)->generateToken($user, 'login-user'),
+                'name'  => $user->name,
+                'email' => $user->email
+            ],
+            'status' => 'success'
+        ]);
     }
 }
