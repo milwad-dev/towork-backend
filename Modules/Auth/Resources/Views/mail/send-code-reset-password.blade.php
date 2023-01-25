b4ed557b93d28fbc628ac7d06664a2da1a9028ba
miladev-ent <milwad.dev@gmail.com>
2023-01-25 16:40:31 +0330

    change send code reset password mail

--- Modules/Auth/Resources/Views/mail/send-code-reset-password.blade.php
+++ Modules/Auth/Resources/Views/mail/send-code-reset-password.blade.php
@@ -1,11 +1,11 @@
 <x-mail::message>
-# Introduction
+# Your account password recovery code in {{ config('app.name') }}
 
-The body of your message.
+This email is at your request to recover the password on the site {{ config('app.name') }} Sent to you.
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+<x-mail::panel>
+    Your password recovery code: {{ $code }}
+</x-mail::panel>
 
 Thanks,<br>
 {{ config('app.name') }}
