15f96ea13e7375ce576abb39e10aa644458b333f
miladev-ent <milwad.dev@gmail.com>
2023-01-25 16:22:39 +0330

    add ForgotPasswordController with form request

---
+++ Modules/Auth/Http/Controllers/ForgotPasswordController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Auth\Http\Controllers;
+
+use Modules\Auth\Http\Requests\ForgotPasswordRequest;
+use Modules\Common\Http\Controllers\Controller;
+
+class ForgotPasswordController extends Controller
+{
+    public function __invoke(ForgotPasswordRequest $request)
+    {
+
+    }
+}
---
+++ Modules/Auth/Http/Requests/ForgotPasswordRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Auth\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
+use Milwad\LaravelValidate\Rules\ValidPhoneNumber;
+use Milwad\LaravelValidate\Rules\ValidStrongPassword;
+
+/**
+ * @property $email
+ */
+class ForgotPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check() === false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => ['required', 'email', 'min:3', 'max:250', 'exists::users,email'],
+        ];
+    }
+}
