2e606daf7d2bfaf9db18b133e9824069799ce0c0
miladev-ent <milwad.dev@gmail.com>
2023-01-25 16:32:36 +0330

    complete ForgotPasswordController

--- Modules/Auth/Http/Controllers/ForgotPasswordController.php
+++ Modules/Auth/Http/Controllers/ForgotPasswordController.php
@@ -3,12 +3,26 @@
 namespace Modules\Auth\Http\Controllers;
 
 use Modules\Auth\Http\Requests\ForgotPasswordRequest;
+use Modules\Auth\Services\ResetPasswordService;
 use Modules\Common\Http\Controllers\Controller;
 
 class ForgotPasswordController extends Controller
 {
-    public function __invoke(ForgotPasswordRequest $request)
+    /**
+     * Send code for reset password.
+     *
+     * @param ForgotPasswordRequest $request
+     * @param ResetPasswordService $resetPasswordService
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function __invoke(ForgotPasswordRequest $request, ResetPasswordService $resetPasswordService)
     {
+        $resetPasswordService->deleteByEmail($request->email); // Delete all old code that user send before.
+        $code = $resetPasswordService->store($request->validated()); // Store code.
 
+        return response([
+            'data' => ['message' => 'Forgot password email sent successfully.'],
+            'status' => 'success'
+        ]);
     }
 }
