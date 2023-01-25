93c36842bc9cf2af8d222d4269a3b3e5980090bf
miladev-ent <milwad.dev@gmail.com>
2023-01-25 16:34:33 +0330

    send email in SendCodeResetPasswordJob

---
+++ Modules/Auth/Jobs/SendCodeResetPasswordJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Modules\Auth\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use Modules\Auth\Mail\SendCodeResetPassword;
+
+class SendCodeResetPasswordJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public string $email,
+        public int $code
+    ) {}
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->email)
+            ->send(new SendCodeResetPassword($this->code));
+    }
+}
