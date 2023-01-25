a6e3d86f70ec367ba26918f81d42eeb2954f1b6c
miladev-ent <milwad.dev@gmail.com>
2023-01-25 16:20:02 +0330

    load migration auth service provider

--- Modules/Auth/Providers/AuthServiceProvider.php
+++ Modules/Auth/Providers/AuthServiceProvider.php
@@ -14,8 +14,29 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerRoutes();
+        $this->registerMigrations();
+    }
+
+    /**
+     * Load route files.
+     *
+     * @return void
+     */
+    private function registerRoutes(): void
+    {
         Route::middleware('api')
             ->prefix('api')
             ->group(__DIR__ . '/../Routes/auth_routes.php');
+    }
+
+    /**
+     * Load migration files.
+     *
+     * @return void
+     */
+    private function registerMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 }
