45e5c7f4b2c8a92513151dc9d2ef9cb4c535dd0f
miladev-ent <milwad.dev@gmail.com>
2023-01-28 17:46:31 +0330

    add ResetCodePasswordRepoEloquent

--- Modules/Auth/Http/Controllers/Password/CodeCheckController.php
+++ Modules/Auth/Http/Controllers/Password/CodeCheckController.php
@@ -2,12 +2,14 @@
 
 namespace Modules\Auth\Http\Controllers\Password;
 
+use Modules\Auth\Http\Requests\Password\CodeCheckRequest;
+use Modules\Auth\Repositories\ResetCodePasswordRepoEloquent;
 use Modules\Common\Http\Controllers\Controller;
 
 class CodeCheckController extends Controller
 {
-    public function __invoke()
+    public function __invoke(CodeCheckRequest $request)
     {
-
+        $resetCodePassword = resolve(ResetCodePasswordRepoEloquent::class)->findByCode($request->code);
     }
 }
---
+++ Modules/Auth/Repositories/ResetCodePasswordRepoEloquent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Auth\Repositories;
+
+use Modules\Auth\Models\ResetCodePassword;
+
+class ResetCodePasswordRepoEloquent
+{
+    /**
+     * Find by code.
+     *
+     * @param  string $code
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model
+     */
+    public function findByCode(string $code)
+    {
+        return ResetCodePassword::query()
+            ->where('code', $code)
+            ->firstOrFail();
+    }
+}
