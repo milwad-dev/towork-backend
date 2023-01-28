2ca8a059977800741041d474f22c747c362286d7
miladev-ent <milwad.dev@gmail.com>
2023-01-28 18:02:44 +0330

    complete ResetPasswordController

--- Modules/Auth/Http/Controllers/Password/ResetPasswordController.php
+++ Modules/Auth/Http/Controllers/Password/ResetPasswordController.php
@@ -6,16 +6,35 @@ use Modules\Auth\Http\Requests\Password\ResetPasswordRequest;
 use Modules\Auth\Repositories\ResetCodePasswordRepoEloquent;
 use Modules\Auth\Services\ResetPasswordService;
 use Modules\Common\Http\Controllers\Controller;
+use Modules\User\Repositories\UserRepoEloquent;
 
 class ResetPasswordController extends Controller
 {
+    /**
+     * Reset password.
+     *
+     * @param  ResetPasswordRequest $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
     public function __invoke(ResetPasswordRequest $request)
     {
         $resetCodePassword = resolve(ResetCodePasswordRepoEloquent::class)->findByCode($request->code); // Find code
-        if ($resetCodePassword->created_at > now()->addHour()) { // Check
-            return resolve(ResetPasswordService::class)->deleteResetCodePasswordWithReturnResponse($resetCodePassword);
-        }
-        
 
+        if ($resetCodePassword->created_at > now()->addHour()) { // Check
+            return resolve(ResetPasswordService::class)
+                ->deleteResetCodePasswordWithReturnResponse($resetCodePassword);
+        }
+
+        $user = resolve(UserRepoEloquent::class)->findByEmail($resetCodePassword->email); // Find user
+        $user->update(['password' => $request->only('password')]); // Update password
+
+        $resetCodePassword->delete(); // Delete reset code password
+
+        return response([
+            'data' => [
+                'message' => 'Password has been successfully updated'
+            ],
+            'status' => 'success'
+        ]);
     }
 }
--- Modules/Auth/Http/Requests/Password/ResetPasswordRequest.php
+++ Modules/Auth/Http/Requests/Password/ResetPasswordRequest.php
@@ -3,6 +3,7 @@
 namespace Modules\Auth\Http\Requests\Password;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
 use Milwad\LaravelValidate\Rules\ValidStrongPassword;
 
 /**
@@ -32,5 +33,17 @@ class ResetPasswordRequest extends FormRequest
             'code'      => ['required', 'numeric', 'between:100000,999999', 'exists:reset_code_passwords,code'],
             'password'  => ['required', 'string', 'min:8', 'max:250', new ValidStrongPassword()]
         ];
+    }
+
+    /**
+     * Prepand for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'password' => Hash::make($this->password)
+        ]);
     }
 }
