d0ffca8fab37fa4ff3fa85336df1436a2fea43f7
miladev-ent <milwad.dev@gmail.com>
2023-01-28 17:55:43 +0330

    add ResetPasswordRequest

--- Modules/Auth/Http/Controllers/Password/ResetPasswordController.php
+++ Modules/Auth/Http/Controllers/Password/ResetPasswordController.php
@@ -2,11 +2,12 @@
 
 namespace Modules\Auth\Http\Controllers\Password;
 
+use Modules\Auth\Http\Requests\Password\ResetPasswordRequest;
 use Modules\Common\Http\Controllers\Controller;
 
 class ResetPasswordController extends Controller
 {
-    public function __invoke()
+    public function __invoke(ResetPasswordRequest $request)
     {
 
     }
---
+++ Modules/Auth/Http/Requests/Password/ResetPasswordRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Auth\Http\Requests\Password;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Milwad\LaravelValidate\Rules\ValidStrongPassword;
+
+/**
+ * @property $code
+ * @property $password
+ */
+class ResetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check() === false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'code'      => ['required', 'numeric', 'between:100000,999999', 'exists:reset_code_passwords,code'],
+            'password'  => ['required', 'string', 'min:8', 'max:250', new ValidStrongPassword()]
+        ];
+    }
+}
