658a6cb45f00728202fbb58ff92176bb3f49d8c0
miladev-ent <milwad.dev@gmail.com>
2023-01-29 20:10:55 +0330

    add user_can_register test

--- Modules/Auth/Tests/Feature/RegisterTest.php
+++ Modules/Auth/Tests/Feature/RegisterTest.php
@@ -6,5 +6,33 @@ use Tests\TestCase;
 
 class RegisterTest extends TestCase
 {
+    /**
+     * Test user can register.
+     *
+     * @test
+     * @return void
+     */
+    public function user_can_register()
+    {
+        $name  = $this->faker->name;
+        $email = $this->faker->email;
 
+        $response = $this->postJson(route('register'), [
+            'name'      => $name,
+            'email'     => $email,
+            'phone'     => 1111111111,
+            'password'  => 'Milwad123!'
+        ]);
+        $response->assertCreated();
+        $response->assertJsonStructure([
+            'data' => [
+                'user',
+                'token'
+            ],
+            'status'
+        ]);
+
+        $this->assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
+        $this->assertDatabaseCount('users', 1);
+    }
 }
