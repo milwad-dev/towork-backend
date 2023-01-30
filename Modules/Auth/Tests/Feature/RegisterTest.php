b9f4621b55bcb9315a0c13718f51f2d2af53ac16
miladev-ent <milwad.dev@gmail.com>
2023-01-30 14:52:35 +0330

    add exists_user_can_not_register test

--- Modules/Auth/Tests/Feature/RegisterTest.php
+++ Modules/Auth/Tests/Feature/RegisterTest.php
@@ -4,6 +4,7 @@ namespace Modules\Auth\Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Modules\User\Models\User;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
@@ -35,6 +36,38 @@ class RegisterTest extends TestCase
                 'token'
             ],
             'status'
+        ]);
+
+        $this->assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
+        $this->assertDatabaseCount('users', 1);
+    }
+
+    /**
+     * Test exists user can not register.
+     *
+     * @test
+     * @return void
+     */
+    public function exists_user_can_not_register()
+    {
+        $name  = 'Milwad';
+        $email = 'milwad.dev@gmail.com';
+
+        User::factory()->create(['name' => $name, 'email' => $email]);
+
+        $response = $this->postJson(route('auth.register'), [
+            'name'      => $name,
+            'email'     => $email,
+            'phone'     => 1111111111,
+            'password'  => 'Milwad123!'
+        ]);
+        $response->assertUnprocessable();
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'name'  => [],
+                'email' => [],
+            ]
         ]);
 
         $this->assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
