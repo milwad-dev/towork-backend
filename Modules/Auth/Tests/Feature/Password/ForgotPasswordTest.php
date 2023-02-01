b9bfb969e5e59140ce2bc597421bf06f064c4fa7
miladev-ent <milwad.dev@gmail.com>
2023-02-01 19:33:49 +0330

    add guest_user_can_see_store_reset_password_code_in_forgot_page test

--- Modules/Auth/Tests/Feature/Password/ForgotPasswordTest.php
+++ Modules/Auth/Tests/Feature/Password/ForgotPasswordTest.php
@@ -6,5 +6,14 @@ use Tests\TestCase;
 
 class ForgotPasswordTest extends TestCase
 {
+    /**
+     * Test guest user can see store reset password code in forgot page.
+     *
+     * @test
+     * @return void
+     */
+    public function guest_user_can_see_store_reset_password_code_in_forgot_page()
+    {
 
+    }
 }
