27a43d262dac3d3e0fc389c0f149343b6c303e0e
miladev-ent <milwad.dev@gmail.com>
2023-02-03 09:31:58 +0330

    add ForgotPasswordTest

---
+++ Modules/Auth/Tests/Feature/Password/CodeCheckTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Auth\Tests\Feature\Password;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\User\Models\User;
+use Tests\TestCase;
+
+class CodeCheckTest extends TestCase
+{
+    use RefreshDatabase;
+
+}
