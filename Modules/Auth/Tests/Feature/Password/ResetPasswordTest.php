0d5c663a836c6116a08b621ce91c235a8cb2f915
miladev-ent <milwad.dev@gmail.com>
2023-02-05 10:18:09 +0330

    add ResetPasswordTest

---
+++ Modules/Auth/Tests/Feature/Password/ResetPasswordTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Auth\Tests\Feature\Password;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ResetPasswordTest extends TestCase
+{
+    use RefreshDatabase;
+}
