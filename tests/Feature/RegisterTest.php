2a57ade04d8ab28a481e22fddbe4454c997dc5e9
miladev-ent <milwad.dev@gmail.com>
2023-02-09 17:47:06 +0330

    install pest faker plugin with add register test

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -1,7 +1,26 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Faker\faker;
 
-    $response->assertStatus(200);
+uses(RefreshDatabase::class);
+
+test('register test', function ()  {
+    $name  = faker()->name;
+    $email = faker()->email;
+
+    $response = $this->postJson(route('auth.register'), [
+        'name'      => $name,
+        'email'     => $email,
+        'phone'     => 1111111111,
+        'password'  => 'Milwad123!'
+    ]);
+    $response->assertCreated();
+    $response->assertJsonStructure([
+        'data' => [
+            'user',
+            'token'
+        ],
+        'status'
+    ]);
 });
