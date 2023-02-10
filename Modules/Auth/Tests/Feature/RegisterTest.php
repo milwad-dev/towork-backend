b01dfd499263e196d4eeda786137a79477e8f031
miladev-ent <milwad.dev@gmail.com>
2023-02-10 12:39:19 +0330

    replace new register test to old test

--- Modules/Auth/Tests/Feature/RegisterTest.php
+++ Modules/Auth/Tests/Feature/RegisterTest.php
@@ -1,76 +1,67 @@
 <?php
 
-namespace Modules\Auth\Tests\Feature;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Modules\User\Models\User;
-use Tests\TestCase;
+use function Pest\Faker\faker;
+use function Pest\Laravel\{postJson}; // Methods
+use function Pest\Laravel\{assertDatabaseHas, assertDatabaseCount, assertDatabaseMissing}; // DB Asserts
 
-class RegisterTest extends TestCase
-{
-    use WithFaker;
-    use RefreshDatabase;
+/*
+ * Use refresh database for truncate database for each test.
+ */
+uses(RefreshDatabase::class);
 
-    /**
-     * Test user can register.
-     *
-     * @test
-     * @return void
-     */
-    public function user_can_register()
-    {
-        $name  = $this->faker->name;
-        $email = $this->faker->email;
+/*
+ * Test user can register with valid data.
+ */
+test('test user can register', function ()   {
+    $name  = faker()->name;
+    $email = faker()->email;
 
-        $response = $this->postJson(route('auth.register'), [
-            'name'      => $name,
-            'email'     => $email,
-            'phone'     => 1111111111,
-            'password'  => 'Milwad123!'
-        ]);
-        $response->assertCreated();
-        $response->assertJsonStructure([
-            'data' => [
-                'user',
-                'token'
-            ],
-            'status'
-        ]);
+    $response = postJson(route('auth.register'), [
+        'name'      => $name,
+        'email'     => $email,
+        'phone'     => 1111111111,
+        'password'  => 'Milwad123!'
+    ]);
+    $response->assertCreated();
+    $response->assertJsonStructure([
+        'data' => [
+            'user',
+            'token'
+        ],
+        'status'
+    ]);
 
-        $this->assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
-        $this->assertDatabaseCount('users', 1);
-    }
+    assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
+    assertDatabaseCount('users', 1);
+});
 
-    /**
-     * Test exists user can not register.
-     *
-     * @test
-     * @return void
-     */
-    public function exists_user_can_not_register()
-    {
-        $name  = 'Milwad';
-        $email = 'milwad.dev@gmail.com';
+/*
+ * Test exists user can't register.
+ */
+test('test exists user can not register',  function ()   {
+    $name  = 'Milwad';
+    $email = 'milwad.dev@gmail.com';
 
-        User::factory()->create(['name' => $name, 'email' => $email]);
+    User::factory()->create(['name' => $name, 'email' => $email]);
 
-        $response = $this->postJson(route('auth.register'), [
-            'name'      => $name,
-            'email'     => $email,
-            'phone'     => 1111111111,
-            'password'  => 'Milwad123!'
-        ]);
-        $response->assertUnprocessable();
-        $response->assertJsonStructure([
-            'message',
-            'errors' => [
-                'name'  => [],
-                'email' => [],
-            ]
-        ]);
+    $response = postJson(route('auth.register'), [
+        'name'      => $name,
+        'email'     => $email,
+        'phone'     => 1111111111,
+        'password'  => 'Milwad123!'
+    ]);
+    $response->assertUnprocessable();
+    $response->assertJsonStructure([
+        'message',
+        'errors' => [
+            'name'  => [],
+            'email' => [],
+        ]
+    ]);
 
-        $this->assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
-        $this->assertDatabaseCount('users', 1);
-    }
-}
+    assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
+    assertDatabaseMissing('users', ['phone' => 1111111111]);
+    assertDatabaseCount('users', 1);
+});
