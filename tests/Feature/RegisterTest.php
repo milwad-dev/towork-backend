5378c8b7f306899a0a64272699ce897d6216056d
miladev-ent <milwad.dev@gmail.com>
2023-02-10 12:34:59 +0330

    add exists user can not register test

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -1,15 +1,14 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\User\Models\User;
 use function Pest\Faker\faker;
 use function Pest\Laravel\{postJson};
-use function Pest\Laravel\{assertDatabaseHas, assertDatabaseCount};
+use function Pest\Laravel\{assertDatabaseHas, assertDatabaseCount, assertDatabaseMissing};
 
 uses(RefreshDatabase::class);
 
-
-
-test('test user can register', static function ()   {
+test('test user can register', function ()   {
     $name  = faker()->name;
     $email = faker()->email;
 
@@ -29,5 +28,32 @@ test('test user can register', static function ()   {
     ]);
 
     assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
+    assertDatabaseCount('users', 1);
+});
+
+
+test('exists user can not register',  function ()   {
+    $name  = 'Milwad';
+    $email = 'milwad.dev@gmail.com';
+
+    User::factory()->create(['name' => $name, 'email' => $email]);
+
+    $response = postJson(route('auth.register'), [
+        'name'      => $name,
+        'email'     => $email,
+        'phone'     => 1111111111,
+        'password'  => 'Milwad123!'
+    ]);
+    $response->assertUnprocessable();
+    $response->assertJsonStructure([
+        'message',
+        'errors' => [
+            'name'  => [],
+            'email' => [],
+        ]
+    ]);
+
+    assertDatabaseHas('users', ['name' => $name, 'email' => $email]);
+    assertDatabaseMissing('users', ['phone' => 1111111111]);
     assertDatabaseCount('users', 1);
 });
