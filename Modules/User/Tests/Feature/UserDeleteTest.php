271cefb8c9370760492c01764bee63bda349cf7d
amir-ys <amir.you74@gmail.com>
2023-02-11 00:43:33 +0330

    add new user (index,update, destroy) tests instead old tests

--- Modules/User/Tests/Feature/UserDeleteTest.php
+++ Modules/User/Tests/Feature/UserDeleteTest.php
@@ -1,33 +1,34 @@
 <?php
 
-namespace Modules\User\Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modules\User\Models\User;
 use Tests\TestCase;
+use function Pest\Faker\faker;
+use function Pest\Laravel\{postJson}; // Methods
+use function Pest\Laravel\{actingAs, assertDatabaseHas, assertDatabaseCount, assertDatabaseMissing};
 
-class UserDeleteTest extends TestCase
-{
-    use RefreshDatabase;
+// DB Asserts
 
-    /**
-     * Test admin user can delete user.
-     *
-     * @test
-     * @return void
-     */
-    public function admin_user_can_delete_user()
-    {
-        $user = User::factory()->create();
-        $deleteUser = User::factory()->create(['email' => $email = 'delete@gmail.com']);
+/*
+ * Use refresh database for truncate database for each test.
+ */
+uses(RefreshDatabase::class);
 
-        // TODO ADD ROLE
-        $this->actingAs($user)
-            ->deleteJson(route('users.destroy', $deleteUser->id))
-            ->assertNoContent();
+/**
+ * Use Testcase to add some requirements.
+ */
+uses(TestCase::class);
 
-        // DB asserts
-        $this->assertDatabaseMissing('users', ['email' => $email]);
-        $this->assertDatabaseCount('users', 1);
-    }
-}
+test('test admin user can delete user' , function (){
+    $user = User::factory()->create();
+
+    $deleteUser = User::factory()->create(['email' => $email = 'delete@gmail.com']);
+
+    $response = actingAs($user)->deleteJson(route('users.destroy', $deleteUser->id));
+    $response->assertJson(['status' => 'success']);
+
+    assertDatabaseMissing('users', ['email' => $email]);
+    assertDatabaseCount('users', 1);
+});
+
