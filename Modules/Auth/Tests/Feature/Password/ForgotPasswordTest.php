e03925f5b22217687a8360779eace528cda96c0e
amir-ys <amir.you74@gmail.com>
2023-02-14 22:10:42 +0330

    add new ForgetPasswordTest instead old test

--- Modules/Auth/Tests/Feature/Password/ForgotPasswordTest.php
+++ Modules/Auth/Tests/Feature/Password/ForgotPasswordTest.php
@@ -3,59 +3,51 @@
 namespace Modules\Auth\Tests\Feature\Password;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Auth\Models\ResetCodePassword;
 use Modules\User\Models\User;
 use Tests\TestCase;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseMissing;
+use function Pest\Laravel\postJson;
 
-class ForgotPasswordTest extends TestCase
-{
-    use RefreshDatabase;
+uses(TestCase::class);
+uses(RefreshDatabase::class);
 
-    /**
-     * Test guest user can see store reset password code in forgot page.
-     *
-     * @test
-     * @return void
-     */
-    public function guest_user_can_see_store_reset_password_code_in_forgot_page()
-    {
-        User::factory()->create(['email' => 'milwad.dev@gmail.com']);
 
-        $response = $this->postJson(route('auth.forgot_password'), ['email' => 'milwad.dev@gmail.com']);
-        $response->assertOk();
-        $response->assertJsonStructure([
-            'data' => [
-                'message'
-            ],
-            'status'
-        ]);
+test('test guest user can see store reset password code in forgot page'  ,function (){
+    $user  = User::factory()->create();
+    $oldResetPasswordCode = ResetCodePassword::factory()->create(['email' => $user->email]);
 
-        $this->assertDatabaseCount('reset_code_passwords', 1);
-        $this->assertDatabaseCount('users', 1);
-
-//        Queue::fake();
-//        Queue::assertPushed(SendCodeResetPasswordJob::class);
-    }
-
-    /**
-     * Test login user can not see store reset password code in forgot page.
-     *
-     * @test
-     * @return void
-     */
-    public function login_user_can_not_see_store_reset_password_code_in_forgot_page()
-    {
-        $user = User::factory()->create(['email' => 'milwad.dev@gmail.com']);
-
-        $response = $this->actingAs($user)->postJson(route('auth.forgot_password'), ['email' => 'milwad.dev@gmail.com']);
-        $response->assertStatus(400);
-        $response->assertJsonStructure([
+    $response = postJson(route('auth.forgot_password'), ['email' => $user->email]);
+    $response->assertOk();
+    $response->assertJsonStructure([
+        'data' => [
             'message'
-        ]);
+        ],
+        'status'
+    ]);
 
-        $this->assertDatabaseCount('reset_code_passwords', 0);
-        $this->assertDatabaseCount('users', 1);
+    assertDatabaseMissing('reset_code_passwords',$oldResetPasswordCode->toArray());
+    assertDatabaseCount('reset_code_passwords', 1);
+    assertDatabaseCount('users', 1);
 
 //        Queue::fake();
 //        Queue::assertPushed(SendCodeResetPasswordJob::class);
-    }
-}
+});
+
+test('test login user can not see store reset password code in forgot page'  ,function (){
+    $user = User::factory()->create();
+
+    $response = actingAs($user)->postJson(route('auth.forgot_password'), ['email' => $user->email]);
+    $response->assertStatus(400);
+    $response->assertJsonStructure([
+        'message'
+    ]);
+
+    assertDatabaseCount('reset_code_passwords', 0);
+    assertDatabaseCount('users', 1);
+
+//        Queue::fake();
+//        Queue::assertPushed(SendCodeResetPasswordJob::class);
+});
