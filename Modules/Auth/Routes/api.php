d6640886c2c815b7a6e569d5b54ca2259f858927
miladev-ent <milwad.dev@gmail.com>
2023-02-26 20:13:05 +0330

    add email verify route

--- Modules/Auth/Routes/api.php
+++ Modules/Auth/Routes/api.php
@@ -18,4 +18,6 @@ Route::group(['middleware' => 'guest:sanctum', 'as' => 'auth.'], static function
     $router->post('password/email', ForgotPasswordController::class)->name('forgot_password');
     $router->post('password/code/check', CodeCheckController::class)->name('check_code_password');
     $router->post('password/reset', ResetPasswordController::class)->name('reset_password');
+
+    $router->post('email/verify', null)->name('email.verify');
 });
