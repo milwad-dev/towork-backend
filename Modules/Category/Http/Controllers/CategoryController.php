d509aea6cf6d1c2898f0369672c01f9f842c9d8c
miladev-ent <milwad.dev@gmail.com>
2023-03-15 14:39:10 +0330

    add index function for CategoryController

--- Modules/Category/Http/Controllers/CategoryController.php
+++ Modules/Category/Http/Controllers/CategoryController.php
@@ -3,9 +3,23 @@
 namespace Modules\Category\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Modules\Category\Repositories\CategoryRepo;
+use Modules\Common\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $categories = resolve(CategoryRepo::class)->getLatest(auth()->id())->get();
+
+        return response()->json([
+            'data' => [
+                $categories
+            ],
+            'status' => 'success'
+        ]);
+    }
 }
