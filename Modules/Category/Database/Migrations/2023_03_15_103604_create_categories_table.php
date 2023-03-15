46d704e3a7509b868b843864ac51a77c08996870
miladev-ent <milwad.dev@gmail.com>
2023-03-15 14:09:44 +0330

    design categories migration

--- Modules/Category/Database/Migrations/2023_03_15_103604_create_categories_table.php
+++ Modules/Category/Database/Migrations/2023_03_15_103604_create_categories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
