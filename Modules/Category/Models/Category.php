42c5ff6db1349853bba0fd196a3d06d47c55d24c
miladev-ent <milwad.dev@gmail.com>
2023-03-15 14:12:37 +0330

    add user relation to Category model

--- Modules/Category/Models/Category.php
+++ Modules/Category/Models/Category.php
@@ -3,6 +3,7 @@
 namespace Modules\Category\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\User\Models\User;
 
 class Category extends Model
 {
@@ -12,4 +13,16 @@ class Category extends Model
      * @var string[]
      */
     protected $fillable = ['title', 'user_id'];
+
+    # Relations
+
+    /**
+     * Relation one-to-many, User model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
