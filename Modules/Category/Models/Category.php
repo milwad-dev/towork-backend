c3f085e9fbf7ad8911c8e4a71b666f06b236a635
miladev-ent <milwad.dev@gmail.com>
2023-03-15 14:11:56 +0330

    add fillable for Category model

--- Modules/Category/Models/Category.php
+++ Modules/Category/Models/Category.php
@@ -2,10 +2,14 @@
 
 namespace Modules\Category\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    /**
+     * Fillable columns.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['title', 'user_id'];
 }
