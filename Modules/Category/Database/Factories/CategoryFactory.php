b9ba91c81db616276b6554082a1487861152f41a
amir-ys <amir.you74@gmail.com>
2023-03-17 00:47:03 +0330

    add category model test

---
+++ Modules/Category/Database/Factories/CategoryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Category\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Category\Models\Category;
+use Modules\User\Models\User;
+
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    public function definition(): array
+    {
+        return
+            [
+                'title' => $this->faker->title,
+                'user_id' => User::factory(),
+            ];
+    }
+}
