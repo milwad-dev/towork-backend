821d9f0651de7c27cfc1cf6be8a63f93fcc7c4ad
miladev-ent <milwad.dev@gmail.com>
2023-03-23 01:31:56 +0430

    add `with` for getLatest method and improve type

--- Modules/Category/Repositories/CategoryRepoEloquent.php
+++ Modules/Category/Repositories/CategoryRepoEloquent.php
@@ -9,13 +9,14 @@ class CategoryRepoEloquent
     /**
      * Get the latest categories with filter by user_id.
      *
-     * @param int|string $user_id
+     * @param int $user_id
      *
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function getLatest(int|string $user_id)
+    public function getLatest(int $user_id)
     {
         return Category::query()
+            ->with('user')
             ->where('user_id', $user_id)
             ->latest();
     }
