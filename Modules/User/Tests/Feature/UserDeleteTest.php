fb8eba3319ae15ba7e829c19b72d8eeb376b1732
miladev-ent <milwad.dev@gmail.com>
2023-03-23 20:23:07 +0430

    add `test guest user can not delete user`

--- Modules/User/Tests/Feature/UserDeleteTest.php
+++ Modules/User/Tests/Feature/UserDeleteTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modules\User\Models\User;
 use Tests\TestCase;
 
-use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseMissing};
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseMissing, assertDatabaseHas, deleteJson};
 
 /*
  * Use refresh database for truncate database for each test.
@@ -25,5 +25,15 @@ test('test admin user can delete user', function () {
     $response->assertJson(['status' => 'success']);
 
     assertDatabaseMissing('users', ['email' => $email]);
+    assertDatabaseCount('users', 1);
+});
+
+test('test guest user can not delete user', function () {
+    $deleteUser = User::factory()->create(['email' => $email = 'delete@gmail.com']);
+
+    $response = deleteJson(route('users.destroy', $deleteUser->id));
+    $response->assertUnauthorized();
+
+    assertDatabaseHas('users', ['email' => $email]);
     assertDatabaseCount('users', 1);
 });
