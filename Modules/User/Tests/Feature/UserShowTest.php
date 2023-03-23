23ffda9961a133d1b4862781e12871ce531091a6
miladev-ent <milwad.dev@gmail.com>
2023-03-23 20:44:08 +0430

    add `test admin user can show special user by id`

--- Modules/User/Tests/Feature/UserShowTest.php
+++ Modules/User/Tests/Feature/UserShowTest.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\User\Models\User;
 use Tests\TestCase;
 
-use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, assertDatabaseMissing, deleteJson};
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas};
 
 /*
  * Use refresh database for truncate database for each test.
@@ -14,3 +15,18 @@ uses(RefreshDatabase::class);
  * Use Testcase to add some requirements.
  */
 uses(TestCase::class);
+
+test('test admin user can show special user by id', function () {
+    $adminUser = User::factory()->create();
+    $implcitUser = User::factory()->create();
+
+    $response = actingAs($adminUser)->getJson(route('users.show', $implcitUser->id));
+    $response->assertOk();
+    $response->assertJsonStructure();
+    $response->assertJsonCount(1, 'data');
+
+    // DB asserts
+    assertDatabaseHas('users', ['name' => $adminUser->name]);
+    assertDatabaseHas('users', ['name' => $implcitUser->name]);
+    assertDatabaseCount('users', 2);
+});
