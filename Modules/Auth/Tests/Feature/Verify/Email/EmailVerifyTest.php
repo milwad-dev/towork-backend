d642e98cba035a16b9c7c8119bc2765b372ec9c6
miladev-ent <milwad.dev@gmail.com>
2023-03-26 10:57:00 +0330

    add `test user verify account by email`

--- Modules/Auth/Tests/Feature/Verify/Email/EmailVerifyTest.php
+++ Modules/Auth/Tests/Feature/Verify/Email/EmailVerifyTest.php
@@ -3,12 +3,23 @@
 namespace Modules\Auth\Tests\Feature\Password;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Modules\Auth\Models\ResetCodePassword;
-use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Modules\User\Models\User;
 use Tests\TestCase;
 
-use function Pest\Laravel\assertDatabaseCount;
-use function Pest\Laravel\postJson;
+use function Pest\Laravel\{actingAs};
 
 uses(RefreshDatabase::class);
 uses(TestCase::class);
+
+test('test user verify account by email', function () {
+    $user = User::factory()->create(['email_verified_at' => null]);
+
+    $response = actingAs($user)->postJson(route('auth.verify.email'));
+    $response->assertOk();
+    $response->assertJsonStructure([
+        'data' => [
+            'message'
+        ],
+        'status'
+    ]);
+});
