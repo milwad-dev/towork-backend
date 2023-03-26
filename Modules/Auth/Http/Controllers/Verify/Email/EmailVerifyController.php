7d539ea2e0d35e56df536d8f096463b0b525d5e4
miladev-ent <milwad.dev@gmail.com>
2023-03-26 11:01:47 +0330

    complete `request` function in EmailVerifyController

--- Modules/Auth/Http/Controllers/Verify/Email/EmailVerifyController.php
+++ Modules/Auth/Http/Controllers/Verify/Email/EmailVerifyController.php
@@ -2,10 +2,24 @@
 
 namespace Modules\Auth\Http\Controllers\Verify\Email;
 
+use Illuminate\Http\Request;
 use Modules\Common\Http\Controllers\Controller;
+use Modules\Common\Responses\JsonResponseFacade;
 
 class EmailVerifyController extends Controller
 {
+    public function request(Request $request)
+    {
+        return $request->user()->hasVerifiedEmail() && $request->expectsJson() ?
+            JsonResponseFacade::forbiddenResponse([
+                'data' => [
+                    'message' => 'You already verified'
+                ],
+                'status' => 'error'
+            ]) :
+            $this->sendVerifyEmail();
+    }
+
     public function verify()
     {
         // TODO: Implement __invoke() method.
