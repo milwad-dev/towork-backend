41d1ed89df6174b87bef7bbcd4fd099734ea2a85
miladev-ent <milwad.dev@gmail.com>
2023-04-21 22:33:24 +0330

    add `task` files

---
+++ Modules/Task/Models/Task.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Task\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+}
