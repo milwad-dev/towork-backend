039deb0d0e2978bbddb8d34857b8c2e8e0d51eaa
miladev-ent <milwad.dev@gmail.com>
2023-04-21 22:43:24 +0330

    Create helpers.php

---
+++ Modules/Common/helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+if (! function_exists('get_enum_values')) {
+    /**
+     * Get values from enum file.
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    function get_enum_values(array $data)
+    {
+        $values = [];
+
+        foreach ($data as $value) {
+            $values[] = $value->value;
+        }
+
+        return $values;
+    }
+}
+
