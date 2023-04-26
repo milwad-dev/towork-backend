7d0b2e5896716a27ddfbb95de7c82b2ecc3d30fe
miladev-ent <milwad.dev@gmail.com>
2023-04-26 17:01:42 +0330

    add store function in TaskService

--- Modules/Task/Services/TaskService.php
+++ Modules/Task/Services/TaskService.php
@@ -2,31 +2,19 @@
 
 namespace Modules\Task\Services;
 
-use Modules\Common\Contracts\Interface\ServicesInterface;
 use Modules\Task\Models\Task;
-use Modules\Common\Repositories\CommonRepoEloquent;
 
-class TaskService implements ServicesInterface
+class TaskService
 {
-    private $class = Task::class;
-
-    public function store($request)
+    /**
+     * Create task and return.
+     *
+     * @param  array $data
+     *
+     * @return Task
+     */
+    public function store(array $data): Task
     {
-        return $this->query()->create([
 
-        ]);
-    }
-
-    public function update($request, $id)
-    {
-         return $this->query()->whereId($id)->update([
-
-        ]);
-    }
-
-    private function query()
-    {
-        return CommonRepoEloquent::query($this->class);
     }
 }
-        
