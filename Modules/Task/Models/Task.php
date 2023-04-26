940645356c7a4963642ed7b84e8b19b0aabc6232
miladev-ent <milwad.dev@gmail.com>
2023-04-26 17:04:04 +0330

    add fillable into Task model

--- Modules/Task/Models/Task.php
+++ Modules/Task/Models/Task.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * Fillable columns.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['title', 'description', 'remind_date', 'priority'];
 }
