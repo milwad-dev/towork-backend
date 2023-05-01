351ea57fe26384bddd59041fd8ea709f60385139
miladev-ent <milwad.dev@gmail.com>
2023-05-01 17:48:14 +0330

    add `test login user can see all own tasks`

--- Modules/Task/Tests/Feature/TaskAllTest.php
+++ Modules/Task/Tests/Feature/TaskAllTest.php
@@ -1,2 +1,16 @@
 <?php
 
+use Modules\User\Models\User;
+use function Pest\Laravel\actingAs;
+
+test('test login user can see all own tasks', function () {
+    $user = User::factory()->create();
+
+    $response = actingAs($user)->getJson(route('tasks.index'));
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'data' => [
+            'tasks'
+        ]
+    ]);
+});
