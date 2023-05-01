0c7f59516fb8761a24b28becd7e60b642a7d5184
miladev-ent <milwad.dev@gmail.com>
2023-05-01 17:50:36 +0330

    add `TestCase` and `RefreshDatabase` to TaskAllTest

--- Modules/Task/Tests/Feature/TaskAllTest.php
+++ Modules/Task/Tests/Feature/TaskAllTest.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modules\User\Models\User;
+use Tests\TestCase;
 use function Pest\Laravel\actingAs;
+
+/*
+ * Use refresh database for truncate database for each test.
+ */
+uses(RefreshDatabase::class);
+
+/**
+ * Use Testcase to add some requirements.
+ */
+uses(TestCase::class);
 
 test('test login user can see all own tasks', function () {
     $user = User::factory()->create();
