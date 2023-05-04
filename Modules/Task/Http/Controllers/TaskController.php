89f8fa6e1ad0839a995eb2f13d22fb61a6988c6b
miladev-ent <milwad.dev@gmail.com>
2023-05-04 13:08:44 +0330

    remove `create` and `edit` function in TaskController

--- Modules/Task/Http/Controllers/TaskController.php
+++ Modules/Task/Http/Controllers/TaskController.php
@@ -22,14 +22,6 @@ class TaskController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTaskRequest $request)
@@ -44,15 +36,7 @@ class TaskController extends Controller
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
-
+    
     /**
      * Update the specified resource in storage.
      */
