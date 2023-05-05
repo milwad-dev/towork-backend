d5d4f39cda6d425a486d5d6bb32efbc00db948b1
miladev-ent <milwad.dev@gmail.com>
2023-05-05 21:35:21 +0330

    complete StoreTaskRequest with add `test login user can store tasks successfully`

--- Modules/Task/Http/Requests/StoreTaskRequest.php
+++ Modules/Task/Http/Requests/StoreTaskRequest.php
@@ -3,6 +3,9 @@
 namespace Modules\Task\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+use Modules\Task\Enums\TaskPriorityEnum;
+use Modules\Task\Enums\TaskStatusEnum;
 
 class StoreTaskRequest extends FormRequest
 {
@@ -22,7 +25,11 @@ class StoreTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title'       => 'required|string|min:3|max:255',
+            'description' => 'required|string|min:3',
+            'remind_date' => 'required|string', // TODO: ADD MORE RULES
+            'priority'    => ['required', 'string', new Enum(TaskPriorityEnum::class), 'max:250'],
+            'status'      => ['required', 'string', new Enum(TaskStatusEnum::class), 'max:250'],
         ];
     }
 }
--- Modules/Task/Tests/Feature/TaskStoreTest.php
+++ Modules/Task/Tests/Feature/TaskStoreTest.php
@@ -3,9 +3,44 @@
 /*
  * Use refresh database for truncate database for each test.
  */
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Task\Enums\TaskPriorityEnum;
+use Modules\Task\Enums\TaskStatusEnum;
+use Modules\User\Models\User;
+use Tests\TestCase;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
+
 uses(RefreshDatabase::class);
 
 /**
  * Use Testcase to add some requirements.
  */
 uses(TestCase::class);
+
+test('test login user can store tasks successfully', function () {
+    $user = User::factory()->create();
+
+    $response = actingAs($user)->postJson(route('tasks.store'), [
+        'title'       => fake()->title,
+        'description' => $title = fake()->text,
+        'remind_date' => now(),
+        'priority'    => (string) TaskPriorityEnum::PRIORITY_FOUR->value,
+        'status'      => (string) TaskStatusEnum::STATUS_ACTIVE->value,
+    ]);
+    $response->assertOk();
+    $response->assertJsonStructure([
+        'data',
+        'status'
+    ]);
+
+    // DB Assertations
+    assertDatabaseCount('users', 1);
+    assertDatabaseCount('tasks', 1);
+
+    assertDatabaseHas('users', $user);
+    assertDatabaseHas('tasks', ['title' => $title]);
+});
