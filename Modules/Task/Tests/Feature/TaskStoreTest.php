8a201e58764d98428c802fa75a4a4fdf8486e83a
miladev-ent <milwad.dev@gmail.com>
2023-05-05 21:19:10 +0330

    add `RefreshDatabase` and `TestCase` into TaskStoreTest

--- Modules/Task/Tests/Feature/TaskStoreTest.php
+++ Modules/Task/Tests/Feature/TaskStoreTest.php
@@ -1,2 +1,11 @@
 <?php
 
+/*
+ * Use refresh database for truncate database for each test.
+ */
+uses(RefreshDatabase::class);
+
+/**
+ * Use Testcase to add some requirements.
+ */
+uses(TestCase::class);
