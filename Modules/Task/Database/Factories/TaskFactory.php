beabbad1998da01b4e38118b4671c30395e84466
miladev-ent <milwad.dev@gmail.com>
2023-05-08 20:08:35 +0330

    add TaskFactory

---
+++ Modules/Task/Database/Factories/TaskFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Task\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Task\Enums\TaskPriorityEnum;
+use Modules\Task\Enums\TaskStatusEnum;
+use Modules\Task\Models\Task;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title,
+            'description' => fake()->text,
+            'remind_date' => now(),
+            'priority' => TaskPriorityEnum::PRIORITY_ONE->value,
+            'status' => TaskStatusEnum::STATUS_ACTIVE->value,
+        ];
+    }
+}
--- Modules/Task/Models/Task.php
+++ Modules/Task/Models/Task.php
@@ -4,10 +4,21 @@ namespace Modules\Task\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Task\Database\Factories\TaskFactory;
 
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * Set factory.
+     *
+     * @return TaskFactory
+     */
+    protected static function newFactory()
+    {
+        return TaskFactory::new();
+    }
 
     /**
      * Fillable columns.
