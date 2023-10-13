cb8988f2a593459db2041f28fd4b56f8a021ae9f
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-10-13 15:58:41 +0330

    load task routes

--- Modules/Task/Providers/TaskServiceProvider.php
+++ Modules/Task/Providers/TaskServiceProvider.php
@@ -15,7 +15,7 @@ class TaskServiceProvider extends ServiceProvider
     public function register()
     {
         $this->registerMigrations();
-//        $this->registerRoutes();
+        $this->registerRoutes();
     }
 
     /**
--- Modules/Task/routes/api.php
+++ Modules/Task/routes/api.php
@@ -4,9 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Modules\Task\Http\Controllers\TaskController;
 
 /*
- * User routes.
+ * Task routes.
  */
-
 Route::group(['middleware' => 'auth:sanctum'], static function ($router) {
     $router->apiResource('tasks', TaskController::class);
 });
